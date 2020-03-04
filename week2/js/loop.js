29f035442b1d9bac40390f70ad678896d9cbc52b
Nuber, Claudia Sorina <claudia.nuber@gmail.com>
2020-03-04 11:15:31 +0000

    week2

---
+++ week2/js/loop.js
@@ -0,0 +1,22 @@
+console.log("We are good to go");
+
+//Limit
+const topOfStairs = 10;
+
+for ( let i=0; i<topOfStairs; i++){
+    console.log(`(For) loop I am on step ${i}`);
+}
+console.log(`I am finished the loop and starting the next one`)
+//counter/Iterator/initialiser
+let counter = 0;
+//start of a loop with a condition
+
+
+while (counter < topOfStairs) {
+    //
+console.log(`(While) I am on step ${counter}`);
+//always increment before you leave the loop
+counter++;
+} //end while
+console.log(`I am on step ${counter} the stairs` )
+
